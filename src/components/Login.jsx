7fd980eabdfabc78bc63a245ee0590b666e80f36
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-08 14:01:33 -0500

    Login completed (token validation/storage, redirect)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,46 @@
 import { useState } from 'react'
 import axios from 'axios'
 import jwt from 'jsonwebtoken'
+import { Redirect } from 'react-router-dom'
+import { Profile } from './Profile'
 
-export const Login = () => {
+export const Login = (props) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [message, setMessage] = useState('')
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log("Do axios call!")
+    const handleSubmit = async (e) => {
+        try {
+            e.preventDefault()
+
+            // post to backend with axios
+            const requestBody = { email, password }
+            const serverUrl = process.env.REACT_APP_SERVER_URL
+            const response = await axios.post(`${serverUrl}/api-v1/users/login`, requestBody)
+            console.log(response)
+
+            // destructure response
+            const { token } = response.data
+
+            // save response to localStorage
+            localStorage.setItem('jwtToken', token)
+
+            // decode the jwt token
+            const decoded = jwt.decode(token)
+
+            // set user in App.jsx's state
+            props.setCurrentUser(decoded)
+
+        } catch(err) {
+            if (err.response.status === 400) {
+                setMessage(err.response.data.msg)
+            } else {
+                console.dir(err)
+            }
+        }
     }
+
+    if (props.currentUser) return <Redirect to='/profile' component={Profile} currentUser={props.currentUser} />
 
     return (
         <div>
@@ -34,6 +64,7 @@ export const Login = () => {
                     value={password}
                 />
                 <input
+                    className="button"
                     type='submit'
                     value='Login'
                 />
